635d2d6d569220470b37065905cb37a1c651a2d4
김경민 <kkm07133@naver.com>
2025-03-20 08:26:32 +0900

    Thread Usage

---
+++ multi-thread-test/src/main/java/org/example/ThreadUsage.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class ThreadUsage {
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> {
+            long start = System.currentTimeMillis();
+            System.out.printf("Hello? %dms\n", System.currentTimeMillis() - start);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("Im not ok %dms\n", System.currentTimeMillis() - start);
+        });
+
+        Thread thread2 = new Thread(() -> {
+            long start = System.currentTimeMillis();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.printf("Are you ok? %dms\n", System.currentTimeMillis() - start);
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
