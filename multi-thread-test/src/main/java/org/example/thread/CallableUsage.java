d0fb938af55e56011a61d6ff162411ebabee08d4
김경민 <kkm07133@naver.com>
2025-03-21 19:25:39 +0900

    Callable + Future

---
+++ multi-thread-test/src/main/java/org/example/thread/CallableUsage.java
@@ -0,0 +1,28 @@
+package org.example.thread;
+
+import org.example.thread.util.TimeChecker;
+
+import java.util.concurrent.*;
+
+public class CallableUsage {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Callable<String> callableTask = () -> {
+            TimeChecker.start(1);
+            return "Hello from Callable!";
+        };
+
+        // callable --[submit]--> executorService
+        Future<String> future = executor.submit(callableTask);
+
+        try {
+            System.out.println("Result: " + future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+
+    }
+}
